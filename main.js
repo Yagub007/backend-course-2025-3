65bacdca7f168513912d4edd9563834d0d996766
yyagub <93602119+Yagub007@users.noreply.github.com>
2025-10-07 09:32:24 +0300

    Implement command-line program in main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+import { Command } from "commander";
+import fs from "fs";
+
+const program = new Command();
+
+program
+  .option("-i, --input <path>", "input JSON file")
+  .option("-o, --output <path>", "output file")
+  .option("-d, --display", "display output to console")
+  .option("-a, --airtime <number>", "show only flights with AIR_TIME longer than value", parseFloat)
+  .option("-t, --date", "show FL_DATE before AIR_TIME and DISTANCE");
+
+program.parse(process.argv);
+const options = program.opts();
+
+// ---   '  ---
+if (!options.input) {
+  console.error("Please, specify input file");
+  process.exit(1);
+}
+
+// ---    ---
+if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1);
+}
+
+// ---   ---
+const data = JSON.parse(fs.readFileSync(options.input, "utf-8"));
+let result = data;
+
+// ---      ---
+if (options.airtime) {
+  result = result.filter((item) => item.AIR_TIME && item.AIR_TIME > options.airtime);
+}
+
+// ---     ---
+let outputText = result
+  .map((item) => {
+    const datePart = options.date ? `${item.FL_DATE} ` : "";
+    return `${datePart}${item.AIR_TIME} ${item.DISTANCE}`;
+  })
+  .join("\n");
+
+// ---    ---
+if (options.output) {
+  fs.writeFileSync(options.output, outputText);
+}
+
+// ---    ---
+if (options.display) {
+  console.log(outputText);
+}
+
+// ---     display,  output ---
+if (!options.output && !options.display) {
+  //   
+}
+
